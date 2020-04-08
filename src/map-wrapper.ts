347e8be5894778f0b60650d5883fdc0fa829141d
Michael Crawford <cerealean@me.com>
2020-04-07 23:14:46 -0500

    Added doc comments and changed pack script to prepare

--- src/map-wrapper.ts
+++ src/map-wrapper.ts
@@ -3,6 +3,10 @@ import { Tuple } from './tuple';
 export class MapWrapper<T, K> {
     private readonly _map: Map<T, K>
 
+    /**
+     * 
+     * @param map A map instance to wrap and provide additional functionality to. If not provided, one will be created for you.
+     */
     constructor(map?: Map<T, K>) {
         this._map = !map ? new Map<T, K>() : map;
         if (!this.isMapType(this._map)) {
@@ -10,52 +14,106 @@ export class MapWrapper<T, K> {
         }
     }
 
+    /**
+     * @description Returns the value specified by the given key, or undefined if the key is not found. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     * @param key 
+     */
     public get(key: T): K | undefined {
         return this._map.get(key);
     }
 
+    /**
+     * @description Sets the specified key in the Map to the given value. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     * @param key
+     * @param value 
+     */
     public set(key: T, value: K): void {
         this._map.set(key, value);
     }
 
+    /**
+     * @description Returns whether the specified key can be found in the Map. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     * @param key 
+     */
     public has(key: T): boolean {
         return this._map.has(key);
     }
 
+    /**
+     * @description Returns how many items are in the Map. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     */
     public get size(): number {
         return this._map.size;
     }
 
+    /**
+     * @description Returns a collection of tuples from within the Map. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     */
     public entries(): IterableIterator<[T, K]> {
         return this._map.entries();
     }
 
+    /**
+     * @description Returns a collection of the keys from within the Map. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     */
     public keys(): IterableIterator<T> {
         return this._map.keys();
     }
 
+    /**
+     * @description Returns a collection of values from within the Map. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     */
     public values(): IterableIterator<K> {
         return this._map.values();
     }
 
+    /**
+     * @description Deletes a tuple from the Map specified by the given key. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     * @param key 
+     */
     public delete(key: T): void {
         this._map.delete(key);
     }
 
+    /**
+     * @description Clears all tuples in the Map. This is the same as native Map functionality.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map 
+     */
     public clear(): void {
         this._map.clear();
     }
 
+    /**
+     * @description Returns the Map instance currently wrapped by this object
+     */
     public toMap(): Map<T, K> {
         return this._map;
     }
 
+    /**
+     * @description Loops over tuples within the map, invoking the given function on each tuple. Works similarly to the native forEach function.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach 
+     * @param callbackFn 
+     */
     public forEach(callbackFn: (value: K, key: T, mapWrapper: MapWrapper<T, K>) => void): void {
         this._map.forEach((value, key) => {
             callbackFn(value, key, this);
         });
     }
 
+    /**
+     * @description Will return the first Tuple that evaluates as true by the given function. Works similarly to the native find function.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+     * @param callbackFn 
+     */
     public find(callbackFn: (value: K, key: T, mapWrapper: MapWrapper<T, K>) => boolean): Tuple<T, K> | null {
         let tuple: Tuple<T, K> | null = null;
         this._map.forEach((value, key) => {
@@ -68,6 +126,11 @@ export class MapWrapper<T, K> {
         return tuple;
     }
 
+    /**
+     * @description Returns a new MapWrapper instance with data filtered by the given function. Works similarly to the native filter function.
+     * @see https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/filter 
+     * @param callbackFn 
+     */
     public filter(callbackFn: (value: K, key: T, mapWrapper: MapWrapper<T, K>) => boolean): MapWrapper<T, K> {
         const newMap = new MapWrapper<T, K>();
         this._map.forEach((value, key) => {
@@ -79,6 +142,11 @@ export class MapWrapper<T, K> {
         return newMap;
     }
 
+    /**
+     * @description Creates a new array of the type returned by the given function. Works similarly to the native map function.
+     * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map 
+     * @param callbackFn 
+     */
     public map<M>(callbackFn: (value: K, key: T, mapWrapper: MapWrapper<T, K>) => M): M[] {
         const collection: M[] = [];
 
--- src/tuple.ts
+++ src/tuple.ts
@@ -2,14 +2,23 @@ import { MapWrapper } from './map-wrapper';
 export class Tuple<T, K> {
     constructor(public readonly key: T, public readonly value: K){}
 
+    /**
+     * @description Returns this tuple in the native format
+     */
     public toNativeTuple(): [T, K] {
         return [this.key, this.value];
     }
 
+    /**
+     * Returns a new Map instance with this tuple included within it
+     */
     public toMap(): Map<T, K> {
         return new Map([this.toNativeTuple()]);
     }
 
+    /**
+     * Returns a new MapWrapper instance with this tuple located within it
+     */
     public toMapWrapper(): MapWrapper<T, K> {
         return new MapWrapper<T, K>(this.toMap());
     }
